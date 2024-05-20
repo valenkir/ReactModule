96dac47aac6b1b78bf2ce9124f6ff30d36a767cf
Valentyna Boriak <valenkiri@gmail.com>
2024-05-20 21:19:21 +0300

    Add contacts section

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
+import HeroContacts from "./HeroContacts";
 
 function Main() {
   return (
     <main className="Main-container">
       <Hero />
+      <HeroContacts />
     </main>
   );
 }
